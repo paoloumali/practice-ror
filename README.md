b7897a20a68e6b8bf6b52b128d8ccecc7a02973b
paoloumali <2522851+paoloumali@users.noreply.github.com>
2023-02-23 16:32:25 +0000

    ror running proxied

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 - rails new blog
 - cd blog
 - bundle install
+
+## Local Setup
+
+- add in hosts file `127.0.0.1 ror.paolo.ph`
+
+
--- _d/ruby/entrypoint1.sh
+++ _d/ruby/entrypoint1.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-#cd /usr/src/app/blog
-#gem install rails
-#/usr/src/app/blog/bin/rails server
-#/usr/src/app/blog/bin/rails server
 cd blog
 bundle install
 bundle exec rails s -e development -p 3000 -b '0.0.0.0'
